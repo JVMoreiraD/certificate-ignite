07b2769692f11e94f126c298a6e80d3ed70b96a4
jvmoreirad <jvitormoreirad@hotmail.com>
2022-09-13 18:48:04 -0300

    feat: config file generate with template

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,12 +1,27 @@
 import { APIGatewayProxyHandler } from "aws-lambda"
 import { document } from "../utils/DynamoDbClient"
-
+import { join } from "path";
+import { readFileSync } from "fs"
+import { compile } from "handlebars";
+import * as dayjs from "dayjs"
 interface ICreateCertificate {
     id: string;
     name: string;
     grade: string;
 }
 
+interface ITemplate {
+    id: string;
+    name: string;
+    grade: string;
+    medal: string;
+    date: string;
+}
+const compileTemplate = async (data: ITemplate) => {
+    const filePath = join(process.cwd(), "src", "templates", "certificate.hbs");
+    const html = readFileSync(filePath, "utf-8");
+    return compile(html)(data)
+}
 export const handler: APIGatewayProxyHandler = async (event) => {
     const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
     await document.put({
@@ -24,6 +39,18 @@ export const handler: APIGatewayProxyHandler = async (event) => {
         KeyConditionExpression: "id = :id",
         ExpressionAttributeValues: { ":id": id }
     }).promise()
+
+    const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+    const medal = readFileSync(medalPath, "base64")
+    const data: ITemplate = {
+        id,
+        name,
+        date: dayjs().format("DD/MM/YYYY"),
+        grade,
+        medal,
+    }
+
+    const content = await compileTemplate(data)
     return {
         statusCode: 201,
         body: JSON.stringify(response.Items[0]),
