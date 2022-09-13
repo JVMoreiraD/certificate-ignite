5c1b29b81dc56fe1e8f1104b842af1d5bd53685e
jvmoreirad <jvitormoreirad@hotmail.com>
2022-09-13 14:10:12 -0300

    feat: dynamoDB config

--- serverless.ts
+++ serverless.ts
@@ -4,7 +4,7 @@ import type { AWS } from '@serverless/typescript';
 const serverlessConfiguration: AWS = {
   service: 'certificate-ignite',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild', "serverless-offline"],
+  plugins: ['serverless-esbuild', "serverless-dynamodb-local", "serverless-offline"],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
@@ -19,12 +19,12 @@ const serverlessConfiguration: AWS = {
   },
   // import the function via paths
   functions: {
-    hello: {
-      handler: "src/functions/hello.handler",
+    generateCertificate: {
+      handler: "src/functions/generateCertificate.handler",
       events: [{
         http: {
-          path: "hello",
-          method: "get",
+          path: "generateCertificate",
+          method: "post",
           cors: true
         }
       }]
@@ -42,7 +42,41 @@ const serverlessConfiguration: AWS = {
       platform: 'node',
       concurrency: 10,
     },
+    dynamodb: {
+      stages: ["dev", "local"],
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true,
+      }
+    }
   },
+  resources: {
+    Resources: {
+      dbCertificateUsers: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "users_certificates",
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5
+          },
+          AttributeDefinitions: [
+            {
+              AttributeName: "id",
+              AttributeType: "S"
+            }
+          ],
+          KeySchema: [
+            {
+              AttributeName: "id",
+              KeyType: "HASH"
+            }
+          ]
+        }
+      }
+    }
+  }
 };
 
 module.exports = serverlessConfiguration;
